3b03528037c201da0ce4b0485e7f210d3ac68ee8
ZMiskolci <phoeneix_real@hotmail.com>
2024-04-03 10:51:58 -0700

    Updated repo name

--- hitide_repos/hitide_repos.py
+++ hitide_repos/hitide_repos.py
@@ -4,12 +4,13 @@ from spreadsheet import Interactor
 
 import pytz
 
+
 def main(args=None):
 
     SPREADSHEET_ID = '1wBIbd56mBDYQLKwHtK1Dzk7n6mtHMc9kftiRTQ63NOw'
     hitide_sheet = Interactor(SPREADSHEET_ID)
     
-    reposSheetName = 'repos'
+    reposSheetName = 'Repos'
     repoListColumnName = 'A'
     repoList = hitide_sheet.Reader.GetDataFromSheet(reposSheetName)
     originalDataOfSheet = hitide_sheet.Reader.GetDataFromSheet('TestDev')
@@ -27,6 +28,7 @@ def main(args=None):
     current_month = now.strftime("%Y-%m")
 
     # worksheet.update([[current_month]], "C8")
+
 
 if __name__ == "__main__":
     main()
