622b35fa6f81f1f4c1afebd1c02142e49591e7d5
ZMiskolci <phoeneix_real@hotmail.com>
2024-04-03 10:48:51 -0700

    - Added test runner in the root directory

---
+++ runner.py
@@ -0,0 +1,35 @@
+from hitide_repos.hitide_repos import main
+
+import argparse
+
+
+def parse_args():
+    """
+    Parses the program arguments
+    Returns
+    -------
+    args
+    """
+
+    parser = argparse.ArgumentParser(
+        description='Synchronize umm-v between ops and uat',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument('-ut', '--uat_token',
+                        help='launchpad token file for uat',
+                        required=True,
+                        metavar='')
+
+    parser.add_argument('-ot', '--ops_token',
+                        help='launchpad token file for ops',
+                        required=True,
+                        metavar='')
+
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    args = parse_args()
+    main()
+    
