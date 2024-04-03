3715cf060ffc0f4a6aecccf7f03a6c657d2f6a90
ZMiskolci <phoeneix_real@hotmail.com>
2024-04-03 11:03:05 -0700

    testing reader types

--- spreadsheet/reader.py
+++ spreadsheet/reader.py
@@ -5,9 +5,17 @@ class Reader(GSheetBase):
     
     def GetDataFromSheet(self, sheetName:str):
         worksheet = self.workbook.worksheet(sheetName)
-        data = worksheet.get_all_values()
-        print(f'type: {type(data)}')
-        print(f'data: {data}')
+        data = worksheet.get_all_records()
+        print(f'all record type: {type(data)}')
+        print(f'all record data: {data}')
+        
+        data2 = worksheet.get_all_cells()
+        print(f'all cells type: {type(data2)}')
+        print(f'all cells data: {data2}')
+        
+        data3 = worksheet.col_values(1)
+        print(f'col_values type: {type(data3)}')
+        print(f'col_values data: {data3}')
         return data
     
         
