8edbbfeae7819b4b5fbda5cb6616745a8c20b53d
James Wood <James.F.Wood@jpl.nasa.gov>
2024-06-18 18:04:06 -0700

    test boto3 list lambda

--- start_data_updater.py
+++ start_data_updater.py
@@ -5,6 +5,8 @@ import argparse
 from data_updater.organizer import Organizer
 import config.config
 
+import boto3
+import json
 
 def ParseArguments():
     '''
@@ -43,7 +45,29 @@ def ParseArguments():
     return args
 
 
+def list_lambda_function_details():
+    # Create a Lambda client
+    client = boto3.client('lambda')
+
+    # Initialize an empty list to store function details
+    function_details = []
+
+    # Paginate through all the Lambda functions
+    paginator = client.get_paginator('list_functions')
+    for page in paginator.paginate():
+        for function in page['Functions']:
+            function_name = function['FunctionName']
+            # Get the detailed information for each function
+            function_detail = client.get_function(FunctionName=function_name)
+            function_details.append(function_detail)
+    
 if __name__ == '__main__':
+
+    print('im here')
+    function_details = list_lambda_function_details()
+    for function_detail in function_details:
+        print(json.dumps(function_detail, indent=4))
+
     args = ParseArguments()
     config.config.Config.LaunchpadToken_OPS = args.ops_token
     config.config.Config.LaunchpadToken_UAT = args.uat_token
