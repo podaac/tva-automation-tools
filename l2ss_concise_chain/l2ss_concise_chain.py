aa88293c55c9753afda201933cf05f84d788e988
Simon Liu <69875423+sliu008@users.noreply.github.com>
2024-08-21 15:49:21 -0700

    Feature/l2ss concise chain (#12)
    * add l2ss concise chain automation
    
    * update python libraries and added cmr-umm-updater
    
    * update l2ss concise chain automation
    
    * fix typo
    
    * debug print

--- l2ss_concise_chain/l2ss_concise_chain.py
+++ l2ss_concise_chain/l2ss_concise_chain.py
@@ -18,12 +18,12 @@ def parse_args():
     )
 
     parser.add_argument('-ut', '--uat_token',
-                        help='launchpad token file for uat',
+                        help='launchpad token for uat',
                         required=False,
                         metavar='')
 
     parser.add_argument('-ot', '--ops_token',
-                        help='launchpad token file for ops',
+                        help='launchpad token for ops',
                         required=False,
                         metavar='')
 
@@ -45,6 +45,7 @@ def sync_association(url_prefix, service_concept_id, headers, current_associatio
         for assoc_concept_id in add:
             resp = create_assoc.add_association(url_prefix, service_concept_id, assoc_concept_id, headers, timeout=timeout)
             if resp.status_code != 200:
+                print(resp.content)
                 print("Failed add association: concept_id being associated may not be valid: %s", assoc_concept_id)
             else:
                 print("Succesfully add association: concept_id: ", assoc_concept_id)
@@ -62,18 +63,10 @@ def sync_association(url_prefix, service_concept_id, headers, current_associatio
 
 if __name__ == '__main__':
 
-    _args = parse_args()
+    args = parse_args()
 
-    with open(_args.uat_token) as file:
-        uat_token_file = json.load(file)
-        uat_token = uat_token_file.get('token')
-
-    with open(_args.ops_token) as file:
-        ops_token_file = json.load(file)
-        ops_token = uat_token_file.get('token')
-
-    ops_headers = {'Authorization': ops_token}
-    uat_headers = {'Authorization': uat_token}
+    ops_headers = {'Authorization': args.ops_token}
+    uat_headers = {'Authorization': args.uat_token}
 
     uat_l2ss_service_concept_id = cmr.queries.ServiceQuery(mode=cmr.queries.CMR_UAT).provider('POCLOUD').name('PODAAC L2 Cloud Subsetter').get()[0].get('concept_id')
     uat_l2ss_collections = create_assoc.current_association(uat_l2ss_service_concept_id, "https://cmr.uat.earthdata.nasa.gov", uat_headers)
