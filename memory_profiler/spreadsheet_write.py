03f5c7cd90796cf85f35f7bf48851b5b1d767c1e
Simon Liu <simon.liu@jpl.nasa.gov>
2024-09-12 09:53:14 -0700

    test

---
+++ memory_profiler/spreadsheet_write.py
@@ -0,0 +1,29 @@
+import gspread
+import csv
+from oauth2client.service_account import ServiceAccountCredentials
+
+# Define the scope for Google Sheets API
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+
+# Path to your service account credentials JSON file
+credentials = ServiceAccountCredentials.from_json_keyfile_name('~/.config/gspread/service_account.json', scope)
+
+# Authorize and create a client
+client = gspread.authorize(credentials)
+
+# Open the Google Sheets by its ID
+spreadsheet_id = os.environ['SPREADSHEET_ID']
+sheet = client.open_by_key(spreadsheet_id)
+
+# Select the first worksheet
+worksheet = sheet.get_worksheet(0)
+
+# Read the CSV file
+with open('collection_statistics.csv', mode='r') as file:
+    reader = csv.reader(file)
+    data = list(reader)
+
+# Write CSV data into the Google Sheet
+worksheet.update('A1', data)
+
+print("CSV data has been written to the Google Sheets.")
