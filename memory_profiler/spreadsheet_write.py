367ecddd61858dbd9f3c00011e40e6c1fdf31974
Simon Liu <69875423+sliu008@users.noreply.github.com>
2024-10-02 12:24:48 -0700

    Feature/memory profiler 2 (#15)
    * add memory profiler workflow
    
    * add service ops profile
    
    * add service ops profile
    
    * add service ops profile
    
    * add service ops profile
    
    * add service ops profile
    
    * add service ops profile
    
    * add service ops profile
    
    * update test
    
    * update test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * update gsheet
    
    * clean up code

---
+++ memory_profiler/spreadsheet_write.py
@@ -0,0 +1,31 @@
+import gspread
+import csv
+import os
+from oauth2client.service_account import ServiceAccountCredentials
+import sys
+
+# Authorize and create a client
+client = gspread.service_account()
+
+# Open the Google Sheets by its ID
+spreadsheet_id = os.environ['SPREADSHEET_ID']
+sheet = client.open_by_key(spreadsheet_id)
+
+# Accept the worksheet name as a command-line argument
+if len(sys.argv) < 2:
+    print("Please provide the worksheet name as a command-line argument.")
+    sys.exit(1)
+
+# Select the worksheet name from the command-line argument
+worksheet_name = sys.argv[1]
+worksheet = sheet.worksheet(worksheet_name)
+
+# Read the CSV file
+with open('collection_statistics.csv', mode='r') as file:
+    reader = csv.reader(file)
+    data = list(reader)
+
+# Write CSV data into the Google Sheet
+worksheet.update('A1', data)
+
+print(f"CSV data has been written to the Google Sheets in worksheet '{worksheet_name}'.")
